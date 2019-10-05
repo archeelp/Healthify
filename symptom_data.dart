d15ad5a0665b7f08c91e0e9ad628fc445d9018d8
yogesh1122554 <48060348+yogesh1122554@users.noreply.github.com>
2019-10-05 17:06:11 +0530

    Add files via upload

---
+++ symptom_data.dart
@@ -0,0 +1,20 @@
+class SymptomQuestionPair {
+  String question;
+  List<String> options;
+  SymptomQuestionPair(this.question, this.options);
+}
+
+var symptomQuestions = [
+  SymptomQuestionPair(
+    "Question 1", 
+    ["Option 1", "Option 2", "Option 3"],
+  ),
+  SymptomQuestionPair(
+    "Question 2", 
+    ["Option 1", "Option 2", "Option 3"],
+  ),
+  SymptomQuestionPair(
+    "Question 3", 
+    ["Option 1", "Option 2", "Option 3"],
+  ),
+];
