a135a04681de7e290212c48364338ce9e73a2646
yogesh1122554 <yogesh1122554@gmail.com>
2019-10-06 02:50:11 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -18,23 +18,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        accentColor: Colors.green,
-        appBarTheme: AppBarTheme(color: Color.fromRGBO(102, 153, 255 , 1))
-      ),
-      debugShowCheckedModeBanner: false,
-      routes: {
-        SymptomScreen.routeName: (ctx)=> SymptomScreen(),
-        SymptomResult.routeName: (ctx)=> SymptomResult(),
-        CityScreen.routeName: (ctx) => CityScreen(),
-        CityDiseasesScreen.routeName: (ctx) => CityDiseasesScreen(DUMMY_DISEASES),
-        Diet.routeName: (ctx) => Diet(),
-        DiseaseDetailScreen.routeName: (ctx) => DiseaseDetailScreen(),
-        TabScreen.routeName: (ctx) => TabScreen(),
-        DietAdvice.routeName: (ctx) => DietAdvice(),
-      }
-    );
+        home: HomeScreen(),
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          accentColor: Colors.green,
+          appBarTheme: AppBarTheme(color: Color.fromRGBO(102, 153, 255, 1)),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+                body1: TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1),
+                ),
+                body2: TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1),
+                ),
+                title: TextStyle(
+                  fontSize: 20,
+                  // fontWeight: FontWeight.bold,
+                  fontFamily: 'RobotoCondensed',
+                ),
+              ),
+        ),
+        debugShowCheckedModeBanner: false,
+        routes: {
+          SymptomScreen.routeName: (ctx) => SymptomScreen(),
+          SymptomResult.routeName: (ctx) => SymptomResult(),
+          CityScreen.routeName: (ctx) => CityScreen(),
+          CityDiseasesScreen.routeName: (ctx) =>
+              CityDiseasesScreen(DUMMY_DISEASES),
+          Diet.routeName: (ctx) => Diet(),
+          DiseaseDetailScreen.routeName: (ctx) => DiseaseDetailScreen(),
+          TabScreen.routeName: (ctx) => TabScreen(),
+          DietAdvice.routeName: (ctx) => DietAdvice(),
+        });
   }
 }
