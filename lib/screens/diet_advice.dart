bb0336c34f1b9aa122e6ba1deea2c96e9f78fa8a
Yash Tailor <49807611+yashtailor@users.noreply.github.com>
2019-10-05 23:02:07 +0530

    Update diet_advice.dart

--- lib/screens/diet_advice.dart
+++ lib/screens/diet_advice.dart
@@ -10,33 +10,85 @@ class DietAdvice extends StatefulWidget {
 class _DietAdviceState extends State<DietAdvice> {
   String text_1 = 'Here are the tips you can follow to overcome your problem:';
   String text_2 = 'Hey!';
-
+  List list1 = ['Eat more frequently.','Choose nutrient-rich foods.','Try smoothies and shakes.','Watch when you drink.','Make every bite count.','Have an occasional treat.','Exercise'];
+  List list2 = ['Eat more fruit, vegetables, nuts, and whole grains.','Exercise, even moderately, for at least 30 minutes a day.','Cut down your consumption of fatty and sugary foods.','Use vegetable-based oils rather than animal-based fats.'];
+  List list3 = ['Consume less “bad” fat and more “good” fat.','Consume less processed and sugary foods.','3.Eat more servings of vegetables and fruits.','4.Eat plenty of dietary fiber.','5. Focus on eating low–glycemic index foods.','6.Get the family involved in your journey.','Engage in regular aerobic activity.','Incorporate a weight training regimen.','9.Focus on reducing daily stress.'];
   Widget build(BuildContext context) {
     int diet_val = ModalRoute.of(context).settings.arguments;
 
-    String show_text_1() {
+    ListTile show_text_1() {
       if (diet_val == 2) {
-        return 'You are perfectly fine, Stay Healthy :)';
+        return ListTile(
+          title: Text(
+            'You are perfectly fine, Stay Healthy :)'
+          ),
+        );
       } else {
-        return text_1;
+        return ListTile(
+          title: Text(
+            text_1
+          ),
+        );
       }
     }
 
-    String show_text_2() {
+    ListView show_text_2() {
       if (diet_val == 2) {
-        return 'How dare I give an advice to healthy ';
+        return ListView(
+          children: <Widget>[
+            ListTile(
+              title: Text('How dare I give an advice to healthy '),
+            )
+          ],
+        );
       } else {
         if (diet_val == 1) {
-          text_2 =
-              '1.Eat more frequently. \n 2.Choose nutrient-rich foods. \n 3.Try smoothies and shakes. \n 4.Watch when you drink. \n 5.Make every bite count. \n 6. Have an occasional treat. \n 7.Exercise';
-        } else if (diet_val == 4) {
-          text_2 =
-              '1. Consume less “bad” fat and more “good” fat. \n 2.Consume less processed and sugary foods. \n 3.Eat more servings of vegetables and fruits. \n 4.Eat plenty of dietary fiber. \n 5. Focus on eating low–glycemic index foods. \n 6.Get the family involved in your journey. \n 7.Engage in regular aerobic activity. \n 8.Incorporate a weight training regimen. \n 9.Focus on reducing daily stress.';
-        } else {
-          text_2 =
-              '1.Eat more fruit, vegetables, nuts, and whole grains. \n 2. Exercise, even moderately, for at least 30 minutes a day. \n 3.Cut down your consumption of fatty and sugary foods. \n 4. Use vegetable-based oils rather than animal-based fats.';
+          Card(
+            elevation: 10,
+            color: Colors.grey,
+            margin: EdgeInsets.all(10),
+              child: ListView(
+              children: <Widget>[
+                for(int i=0;i<list1.length;i++){
+                  ListTile(
+                    title: list1[i],
+                  )
+                }
+              ],
+            ),
+          );
         }
-        return text_2;
+        else if (diet_val == 4) {
+           Card(
+            elevation: 10,
+            color: Colors.grey,
+            margin: EdgeInsets.all(10),
+              child: ListView(
+              children: <Widget>[
+                for(int i=0;i<list3.length;i++){
+                  ListTile(
+                    title: list1[i],
+                  )
+                }
+              ],
+            ),
+          );
+        } else {
+           Card(
+            elevation: 10,
+            color: Colors.grey,
+            margin: EdgeInsets.all(10),
+              child: ListView(
+              children: <Widget>[
+                for(int i=0;i<list2.length;i++){
+                  ListTile(
+                    title: list1[i],
+                  )
+                }
+              ],
+            ),
+          );
+        }
       }
     }
 
@@ -51,19 +103,12 @@ class _DietAdviceState extends State<DietAdvice> {
         padding: EdgeInsets.all(5),
         child: Column(
           children: <Widget>[
-            Text(
-              show_text_1(),
-            ),
+            show_text_1(),
             Container(
               width: double.infinity,
               margin: EdgeInsets.all(10),
               padding: EdgeInsets.all(10),
-              child: Card(
-                elevation: 5,
-                child: Text(
-                  show_text_2(),
-                ),
-              ),
+              child: show_text_2(),
             )
           ],
         ),
