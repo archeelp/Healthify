4c3b0230dd7976da16fbbb2510ed3939f088a8d2
yogesh1122554 <48060348+yogesh1122554@users.noreply.github.com>
2019-10-05 22:38:04 +0530

    Add files via upload

--- lib/screens/city_doctor_screen.dart
+++ lib/screens/city_doctor_screen.dart
@@ -1,7 +1,8 @@
 import 'package:app/widgets/doctor_profile.dart';
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
 import '../models/doctor.dart';
+import 'dart:convert';
 
 class CityDoctorScreen extends StatefulWidget {
   final List<Doctor> doctors;
@@ -16,20 +17,41 @@ class CityDoctorScreenState extends State<CityDoctorScreen> {
   String cityTitle;
   List<Doctor> diplayedDoc;
   var _loadedInitData = false;
-
+  var cityId;
+  final doctorList = [];
   @override
   void didChangeDependencies() {
     if (!_loadedInitData) {
       final routeArgs =
           ModalRoute.of(context).settings.arguments as Map<String, String>;
       cityTitle = routeArgs['title'];
-      final cityId = routeArgs['id'];
+      cityId = routeArgs['id'];
       diplayedDoc = widget.doctors.where((doc) {
         return doc.locationId.contains(cityId);
       }).toList();
       _loadedInitData = true;
     }
     super.didChangeDependencies();
+  }
+
+  Future<void> fetchAndSetProducts() async {
+    final url = 'http://c55b1289.ngrok.io/' + cityTitle;
+    try {
+      final response = await http.get(url);
+      final extractedData = json.decode(response.body) as Map<String, List<String>>;
+      for(int i = 0; i < extractedData["names"].length; i++) {
+        var tempDoc = Doctor(
+          exp: extractedData["exp"][i],
+          name: extractedData["name"][i],
+          specialization: extractedData["q"][i],
+          locationId: cityId,
+          contact: "9987570286"
+        );
+        doctorList.add(tempDoc);
+      }
+    } catch (error) {
+      throw (error);
+    }
   }
 
   @override
