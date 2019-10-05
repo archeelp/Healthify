96ded78788f7ad87914edb05d58433b9200f8236
Archeel Parekh <archeelparekh11@gmail.com>
2019-10-05 23:45:16 +0530

    Add files via upload

--- lib/models/doctor.dart
+++ lib/models/doctor.dart
@@ -4,11 +4,12 @@ class Doctor {
   final String name;
   final String locationId;
   final String specialization;
+  final String experience;
   final String contact;
 
   const Doctor(
       {@required this.name,
       @required this.locationId,
       @required this.specialization,
-      this.contact});
+      this.contact, this.experience});
 }
